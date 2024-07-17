37dc13eab068da8aaad3a826be253a48637010e6
Fili258-oss <marinorubianobotina@gmail.com>
2024-07-17 07:49:28 -0500

    Save product added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Intervention\Image\Laravel\Facades\Image;
 
 class ProductController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        if ($request->image !="") 
+        {
+            $strpos = strpos($request->image,';');
+            $sub = substr($request->image,0,$strpos);
+            $ex = explode('/',$sub)[1];
+            $name = time().".".$ex;
+            $img = Image::read($request->image)->resize(200,200);
+            $upload_path = public_path()."/upload/";
+            $img->save($upload_path . $name);
+            $product->image = $name;
+
+        }else
+        {
+            $product->image = "no-picture.png";
+        }
+        $product->type = $request->type;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->save();
+
+    }
 }
--- resources/js/components/products/Form.vue
+++ resources/js/components/products/Form.vue
@@ -1,33 +1,82 @@
+<script setup>
+import axios from 'axios';
+import { reactive } from 'vue';
+
+    const form = reactive({
+        name: "",
+        description: "",
+        image: "",
+        type: 0,
+        quantity: "",
+        price: ""
+    })
+
+    const getImage = () => {
+        let image = "/upload/no-picture.png"
+        if (form.image) {
+            if (form.image.indexOf("base64") != -1) {
+                image = form.image
+            }else
+            {
+                image = "/upload/" + form.image
+            }
+        }
+        return image
+    }
+
+    const handleFileChange = (e) => {
+        let file = e.target.files[0]
+        let reader = new FileReader()
+        reader.onloadend = (file) => {
+            form.image = reader.result
+        }
+        reader.readAsDataURL(file)
+
+    }
+    const handleSave = () => {
+        e.preventDefault();
+        axios.post('/api/products', form)
+    }
+
+    const handleSaveSuccess = () => {
+        alert("Registro guardado exitosamente!")
+    }
+</script>
 <template>
     <section>
         <form class="row g-3 mt-4">
             <div class="col-md-6">
                 <label for="nameProduct" class="form-label">Nombre del producto</label>
-                <input type="text" class="form-control" id="nameProduct">
+                <input type="text" v-model="form.name" class="form-control" id="nameProduct">
             </div>
             <div class="col-6">
-                <label for="inputAddress2" class="form-label">Precio</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                <label for="priceProduct" class="form-label">Precio</label>
+                <input type="number" v-model="form.price" class="form-control" id="priceProduct" placeholder="Ej:200000">
             </div>
             <div class="col-md-6">
-                <label for="inputCity" class="form-label">Categoría</label>
-                <input type="text" class="form-control" id="inputCity">
+                <label for="categoryProduct" class="form-label">Categoría</label>
+                <select v-model="form.type" class="form-select" name="categoryProduct" id="categoryProduct">
+                    <option value="0">Seleccione una opción</option>
+                    <option value="1">Aseo y jabón</option>
+                    <option value="2">Canasta Familiar</option>
+                </select>
             </div>
             <div class="col-md-6">
                 <label for="inputState" class="form-label">Inventario</label>
-                <input type="number" class="form-control" placeholder="Ej:15">
+                <input type="number" v-model="form.inventary" class="form-control" placeholder="Ej:15">
             </div>
             <div class="col-md-12">
                 <label for="inputPassword4" class="form-label">Descripción del producto</label>
-                <textarea name="descriptionProduct" class="form-control" id="descriptionProduct" placeholder="Agrega tu descripción"></textarea>
+                <textarea v-model="form.description" name="descriptionProduct" class="form-control" id="descriptionProduct" placeholder="Agrega tu descripción"></textarea>
             </div>
             <div class="col-12">
-                <label for="formFileMultiple" class="form-label">Fotografías</label>
-                <input class="form-control" type="file" id="formFileMultiple" multiple>
+                <label for="formFileMultiple" class="form-label">Fotografías</label>                
+                <input type="file" @change="handleFileChange" class="form-control" multiple>
+                <img :src="getImage()" alt="Imagen de producto" class="img-thumbnail mt-2" width="100">
             </div>            
             
             <div class="col-12">
-                <button type="submit" class="btn btn-primary">Guardar</button>
+                <button @click="handleSave" class="btn btn-primary">Guardar</button>
             </div>
         </form>
     </section>
